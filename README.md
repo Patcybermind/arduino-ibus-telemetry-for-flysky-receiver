34158d08aaa03438c52bd87567893d26696c444d
Patcybermind <pato.gonzalezcabrera@gmail.com>
2024-07-09 11:01:28 -0400

    a

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 my goal is to use an arduino to send telemetry data from a flysky receiver to a flysky radio transmitter using ibus that way i can monitor a lot of things like voltage etc
+
+rn you must have pin 28 set to on because if you dont it will go back to boot sel mode so you can reflash something else without having to plug and unplug
--- arduino telemetry.ino
+++ arduino telemetry.ino
@@ -3,7 +3,7 @@ extern "C" {
 #include "pico/time.h"
 #include "pico/bootrom.h"
 }
-
+/*
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT); // Set the built-in LED pin as an output
@@ -16,71 +16,69 @@ void loop () {
   if (digitalRead(28) == HIGH) {
     digitalWrite(LED_BUILTIN, HIGH); // Turn on the LED
   } else {
-    digitalWrite(LED_BUILTIN, LOW); // Turn off the LED
-    delay(50);
-    digitalWrite(LED_BUILTIN, HIGH); // Turn on the LED
-    delay(50);
+    reset_usb_boot(1<<PICO_DEFAULT_LED_PIN,0);
   }
   
   //reset_usb_boot(1<<PICO_DEFAULT_LED_PIN,0);
 }
+*/
 
-
-/*
+// Before including IBusBM, explicitly specify the namespace for HardwareSerial
+//using HardwareSerial = arduino::HardwareSerial;
+#include <SoftwareSerial.h>
 #include <IBusBM.h>
+
+// Define SoftwareSerial RX and TX pins
+
+
+//using HardwareSerial = arduino::HardwareSerial;
+// Create a SoftwareSerial object
+
+
 
 IBusBM IBus; // Use a single IBusBM instance for both servo signal and sensor data
 
+// sensor values
+#define TEMPBASE 400    // base value for 0'C
+uint16_t speed=0;
+uint16_t temp=TEMPBASE+200; // start at 20'C
 
 
 void setup() {
-  Serial.begin(115200);
+  //serial1.begin(115200);
 
-  //#define TEMPBASE 400    // base value for 0'C
+  
 
-// sensor values
-uint16_t speed=0;
-uint16_t temp=TEMPBASE+200; // start at 20'C
- Since Arduino Uno has only one hardware serial port, we use it for both debugging and iBUS.
+  // sensor values
+  uint16_t speed=0;
+  uint16_t temp=TEMPBASE+200; // start at 20'C
+  //Since Arduino Uno has only one hardware serial port, we use it for both debugging and iBUS.
   // Note: This setup requires careful management of serial communication to avoid conflicts.
-  IBus.begin(Ser500; // Use the same Serial for IBus communication
+  IBus.begin(serial1); // Use the same Serial for IBus communication
 
   //Serial.println("Start iBUS");
 
-    // adding 2 sensors
-  IBus.addSensor(IBUSS_RPM);
-  IBus.addSensor(IBUSS_TEMP);
-#define TEMPBASE 400    // base value for 0'C
-
-// sensor values
-uint16_t speed=0;
-uint16_t temp=TEMPBASE+200; // start at 20'C
-
-  // Since Arduino Uno has only one hardware serial port, we use it for both debugging and iBUS.
-  // Note: This setup requires careful management of serial communication to avoid conflicts.
-  IBus.begin(Serial); // Use the same Serial for IBus communication
-
-  //Serial.println("Start iBUS");
-
-    // adding 2 sensors
+  // adding 2 sensors
   IBus.addSensor(IBUSS_RPM);
   IBus.addSensor(IBUSS_TEMP);
 }
-#define TEMPBASE 400    // base value for 0'C
 
-// sensor values
-uint16_t speed=0;
-uint16_t temp=TEMPBASE+200; // start at 20'C
+
 
 void loop() {
   IBus.setSensorMeasurement(1,speed);
-  speed += 10;                         increase motor speed by 10 RPM
-  IBus.setSensorMeasurement(2,temp++);   /increase temperature by 0.1 'C every loop
+  speed += 10;                         //increase motor speed by 10 RPM
+  IBus.setSensorMeasurement(2,temp++);   //increase temperature by 0.1 'C every loop
   //Serial.print("Speed=");
   //Serial.print(speed);
   //Serial.print(" Temp=");
   //Serial.println((temp-TEMPBASE)/10.);
   delay(500);
+  if (digitalRead(28) == HIGH) {
+    digitalWrite(LED_BUILTIN, HIGH); // Turn on the LED
+  } else {
+    reset_usb_boot(1<<PICO_DEFAULT_LED_PIN,0);
+  }
 }
 
 
@@ -95,4 +93,3 @@ int readIBusServoData(int channel) {
   // Return the servo data
   return servoData;
 }
-*/
